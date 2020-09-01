3f51b2bf41678abf3c98f764bc5902223b01e9db
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-09-02 00:11:43 +0100

    rework of rotor contact to use enumeration

--- src/Core/RotorContact.py
+++ src/Core/RotorContact.py
@@ -12,97 +12,34 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 '''
-from Singleton import Singleton
+from enum import Enum
 
 
-# ***********************************************************************
-# Enumeration for converting a rotor pin/contact to a letter
-# ***********************************************************************
-@Singleton
-class RotorContact:
-
-    # Number of rotor contacts.
-    NUMBER_OF_CONTACTS = 26
-
-    # ASCII to contact offset value
-    ASCII_TO_CONTACT_OFFSET = 64
-
-
-    ##
-    # Class initialisation.  Create the contacts dictionary.
-    # @param self The object pointer.
-    def __init__(self):
-        self.__contact = {
-             1 : 'A',
-             2 : 'B',
-             3 : 'C',
-             4 : 'D',
-             5 : 'E',
-             6 : 'F',
-             7 : 'G',
-             8 : 'H',
-             9 : 'I',
-            10 : 'J',
-            11 : 'K',
-            12 : 'L',
-            13 : 'M',
-            14 : 'N',
-            15 : 'O', 
-            16 : 'P',
-            17 : 'Q',
-            18 : 'R',
-            19 : 'S',
-            20 : 'T',
-            21 : 'U',
-            22 : 'V',
-            23 : 'W',
-            24 : 'X',
-            25 : 'Y',
-            26 : 'Z'
-        }
-
-
-    ##
-    # Convert a character [A-Za-z] to it's contact number.  If the parameter
-    # 'character' is not a string of 1 character then a ValueErrorn exception
-    # is generated.
-    # @param self The object pointer.
-    # @param character Character to convert to a contact number.
-    # @return Success = Contact number [1-26] or ValueError exception.
-    def CharacterToContact(self, character):
-        # Verify that the character parameter is of type 'string'.
-        if type(character) != str:
-            raise ValueError("character is not a string")
-
-        # Character has to be a single character, nothing more or less.
-        if len(character) != 1:
-            raise ValueError("character is more than one character!")
-
-        # Get the integer ordinal of a one-character string and convert it to
-        # a key (1 .. 26).
-        asciiValue = ord(character.upper())
-        contactNo = asciiValue - self.ASCII_TO_CONTACT_OFFSET
-
-        # Verify contact is somewhere between 1 and max contact number.
-        if (contactNo < 1) or (contactNo > self.NUMBER_OF_CONTACTS):
-            raise ValueError("invalid contact number")
-
-        # Return the contact number.
-        return contactNo
-
-
-    ##
-    # Convert a contact number to a character [A-Za-z].
-    # @param contact Contact number to convert to a character.
-    # @return Success = character or ValueError exception.
-    def ContactToCharacter(self, contact):
-        # Verify that the contact is of type 'int'.
-        if type(contact) != int:
-            raise ValueError("Contact number is not an integer")
-
-        if (contact < 1) or (contact > self.NUMBER_OF_CONTACTS):
-            raise ValueError("invalid contact value")
-
-        # Convert contact to ascii value then return character.
-        asciiVal = contact + self.ASCII_TO_CONTACT_OFFSET
-        return str(unichr(asciiVal))
+## Enumeration of a rotor contact character (A-Z).
+class RotorContact(Enum):
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+    E = 5
+    F = 6 
+    G = 7
+    H = 8
+    I = 9
+    J = 10
+    K = 11
+    L = 12
+    M = 13
+    N = 14
+    O = 15
+    P = 16
+    Q = 17
+    R = 18
+    S = 19
+    T = 20
+    U = 21
+    V = 22
+    W = 23
+    X = 24
+    Y = 25
+    Z = 26
