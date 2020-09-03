d5ccc1e904eb7a74378514d1d9360380c83492c7
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-09-03 09:53:30 +0100

    tidy of plugboard

--- src/Core/plugboard.py
+++ src/Core/plugboard.py
@@ -12,85 +12,72 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 '''
-from RotorContact import RotorContact
+from rotor_contact import NO_OF_ROTOR_CONTACTS
 
 
 # ***********************************************************************
 # Class representing an Enigma plugboard / Steckerbrett (German).
 # ***********************************************************************
-class Plugboard(object):
+class Plugboard:
 
     # Plugboard plug source in use exception message.
-    PlugboardSrcInUseMsg = 'Source of plugboard ({0}:{1}) is already in use'
+    PlugboardSrcInUseMsg = 'Plugboard Source ({0}:{1}) is already in use'
 
     # Plugboard plug destination in use exception message.
-    PlugboardDesInUseMsg = 'Destination of plugboard ({0}:{1}) is already in use'
+    PlugboardDesInUseMsg = 'Plugboard Destination ({0}:{1}) is already in use'
 
 
-    ##
-    # Class initialisation.
-    # @param self The object pointer.
+    ## Class initialisation.
+    #  @param self The object pointer.
     def __init__(self):
-        self.__plugs = {}
+        self.wiring = {}
 
 
-    ##
-    # Set a plug, giving a source and destination.  Only 1 plug per hole,
-    # or example 2 plugs can't use 'A'. If this, or another exception occurs
-    # then a ValueErrorn exception is generated.
-    # @param self The object pointer.
-    # @param src Source of the plug.
-    # @param src Source of the plug.
-    def SetPlug(self, src, dest):
-        if not self.__ValidatePosition(src):
+    ## Set a plug, giving a source and destination.  Only 1 plug per hole,
+    #  or example 2 plugs can't use 'A'. If this, or another exception occurs
+    #  then a ValueErrorn exception is generated.
+    #  @param self The object pointer.
+    #  @param src Source of the plug.
+    #  @param src Source of the plug.
+    def set_plug(self, src, dest):
+
+        if not (src >= 1) and (src <= NO_OF_ROTOR_CONTACTS):
             raise ValueError("Invalid source plug position")
 
-        if not self.__ValidatePosition(dest):
+        if not (dest >= 1) and (dest <= NO_OF_ROTOR_CONTACTS):
             raise ValueError("Invalid destination plug position")
 
         # Check that source end of the plugboard is not in use.
-        if ((src in  self.__plugs) or (src in self.__plugs.values())):
+        if ((src in  self.wiring) or (src in self.wiring.values())):
             raise ValueError(self.PlugboardSrcInUseMsg.format(src, dest))
 
         # Check that destination end of the plugboard is not in use.
-        if ((dest in  self.__plugs) or (dest in self.__plugs.values())):
+        if ((dest in  self.wiring) or (dest in self.wiring.values())):
             raise ValueError(self.PlugboardDesInUseMsg.format(src, dest))
 
-        self.__plugs[src] = dest
+        self.wiring[src] = dest
 
 
-    ##
-    # Get the other end of a plug.  If there is a plug then work out the other
-    # end, otherwise return same letter.  
-    # @param self The object pointer.
-    # @param src Plug to get other end of.
-    # @return Other end of plug.  For example if 'A' is wired to 'T' then 'A'
-    # becomes 'T', but if 'C' isn't wired then it will return 'C'.
-    def GetPlug(self, src):
-        if not self.__ValidatePosition(src):
+    ## Get the other end of a plug.  If there is a plug then work out the other
+    #  end, otherwise return same letter.
+    #  @param self The object pointer.
+    #  @param src Plug to get other end of.
+    #  @return Other end of plug.  For example if 'A' is wired to 'T' then 'A'
+    #  becomes 'T', but if 'C' isn't wired then it will return 'C'.
+    def get_plug(self, src):
+        if not (src >= 1) and (src <= NO_OF_ROTOR_CONTACTS):
             raise ValueError("Invalid source plug position")
 
         # The output of the plug, it will default to pass-through initially.
-        outPlug = src
+        plug_output = src
 
         # Check that the plug is fitted, if it has then modify the outgoing
         # value, otherwise it will pass straight through, e.g. 'T' will remain
         # as 'T'.
-        if src in  self.__plugs:
-            outPlug = self.__plugs[src]
+        if src in  self.wiring:
+            plug_output = self.wiring[src]
 
-        elif src in self.__plugs.values():
-            outPlug = self.__plugs.keys()[self.__plugs.values().index(src)]
+        elif src in self.wiring.values():
+            plug_output = self.wiring.keys()[self.wiring.values().index(src)]
 
-        return outPlug
-
-
-    ##
-    # Simple validation of the plug position to ensure that it is within bounds
-    # of the 'A' to 'Z'.
-    # @param self The object pointer.
-    # @param plug Number of he plug.
-    # @return True if valid, False if not.
-    def __ValidatePosition(self, plug):
-        return ((plug >= 1) and
-                (plug <= RotorContact.Instance().NUMBER_OF_CONTACTS))
+        return plug_output
