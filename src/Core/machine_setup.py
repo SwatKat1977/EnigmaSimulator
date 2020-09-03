1aac883111de3344159d48e133f2927e68b527c8
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-09-03 20:37:00 +0100

    updates

--- src/Core/machine_setup.py
+++ src/Core/machine_setup.py
@@ -12,31 +12,27 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 '''
+from enum import Enum
+from plugboard import Plugboard
 #from RotorsManager import rotorsManager
 
 
-# ***********************************************************************
-# Enumeration for number of rotors.
-# *********************************************************************** 
-class RotorCount(object):
+## Enumeration for total number of rotors.
+class RotorCount(Enum):
     ThreeRotors = 3
     FourRotors = 4
 
 
-# ***********************************************************************
-# Enumeration for Enigma machine models.
-# *********************************************************************** 
-class EnigmaModel(object):
-    ModelAorB = 'Model A or B'
-    Enigma1 = 'Enigma 1'
-    M3 = 'M3'
-    M4 = 'M4'
+## Enumeration for Enigma machine models.
+class EnigmaModel(Enum):
+    ModelAorB = 0
+    Enigma1 = 1
+    M3 = 2
+    M4 = 3
 
 
-# ***********************************************************************
-# Implementation of machine setup.
-# *********************************************************************** 
-class MachineSetup(object):
+## Implementation of machine setup.
+class MachineSetup:
 
     ##
     # Get model of the Enigma machine.
@@ -45,34 +41,44 @@ class MachineSetup(object):
     def Model(self):
         return self._model
 
-    ##
-    # Get number of rotors for Enigma machine.
-    # @return Number of rotors.
+    ## Get number of rotors for Enigma machine.
+    #  @return Number of rotors.
     @property
     def NumberOfRotors(self):
         return len(self._rotors)
 
-    ##
-    # Get rotors for Enigma machine.
-    # @return list of rotors.
+    ## Get rotors for Enigma machine.
+    #  @return list of rotors.
     @property
     def Rotors(self):
         return self._rotors
 
-    ##
-    # Get the machines plug board.
-    # @return plugboard instance if using one, otherwise None.
+    ## Get the machines plug board.
+    #  @param model Enigma machine mode.
+    #  @return plugboard instance if using one, otherwise None.
     @property
     def Plugboard(self):
         return self._plugboard
 
 
-    ##
-    # Constructor, create a machine setup.
-    # @param model Enigma machine mode.
-    # @param selectedRotors The name of rotors used by the Enigma machine.
-    # @param plugboard Enigma plugboard (None if machine doesn't use one).
-    def __init__(self, model, selectedRotors, plugboard = None):
+    ## Constructor, create a machine setup.
+    #  @param model Enigma machine mode.
+    #  @param selectedRotors The name of rotors used by the Enigma machine.
+    #  @param plugboard Enigma plugboard (None if machine doesn't use one).
+    def __init__(self, model, selectedRotors, plugboard=None):
         self._model = model
         self._plugboard = plugboard
         self._rotors = selectedRotors
+
+        # Check to make sure model is from EnigmaMachine enumeration.
+        if not isinstance(model, (EnigmaModel)):
+            raise ValueError("Invalid Enigma machine model")
+
+
+        # Check to make sure plugboard is a PlugBoard or None.
+        if plugboard is not None:
+            if not isinstance(plugboard, (Plugboard)):
+                raise ValueError("Invalid plugboard object")
+
+
+em = MachineSetup(EnigmaModel.Enigma1, 'tok')
