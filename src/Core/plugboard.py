1d4e4a3793e148b470b6e908e5d640c4747ae647
swatkat1977 <5666718+SwatKat1977@users.noreply.github.com>
2020-09-07 19:25:43 +0100

    fixes to plugboard

--- src/Core/plugboard.py
+++ src/Core/plugboard.py
@@ -12,25 +12,29 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 '''
-from Core.rotor_contact import NO_OF_ROTOR_CONTACTS
+from Core.rotor_contact import RotorContact, NO_OF_ROTOR_CONTACTS
 
 
 # ***********************************************************************
 # Class representing an Enigma plugboard / Steckerbrett (German).
 # ***********************************************************************
 class Plugboard:
+    __slots__ = ['_wiring']
 
     # Plugboard plug source in use exception message.
-    PlugboardSrcInUseMsg = 'Plugboard Source ({0}:{1}) is already in use'
+    PlugboardSrcInUseMsg = 'Plugboard source ({0}:{1}) is already in use'
 
     # Plugboard plug destination in use exception message.
-    PlugboardDesInUseMsg = 'Plugboard Destination ({0}:{1}) is already in use'
+    PlugboardDesInUseMsg = 'Plugboard destination ({0}:{1}) is already in use'
 
 
     ## Class initialisation.
     #  @param self The object pointer.
     def __init__(self):
-        self.wiring = {}
+        self._wiring = {}
+
+        for letter in RotorContact:
+            self._wiring[letter] = letter
 
 
     ## Set a plug, giving a source and destination.  Only 1 plug per hole,
@@ -40,22 +44,24 @@ class Plugboard:
     #  @param src Source of the plug.
     #  @param src Source of the plug.
     def set_plug(self, src, dest):
+        if not isinstance(src, (RotorContact)):
+            raise ValueError("Source plug position not valid")
 
-        if not (src >= 1) and (src <= NO_OF_ROTOR_CONTACTS):
-            raise ValueError("Invalid source plug position")
-
-        if not (dest >= 1) and (dest <= NO_OF_ROTOR_CONTACTS):
-            raise ValueError("Invalid destination plug position")
+        if not isinstance(dest, (RotorContact)):
+            raise ValueError("Destination plug position not valid")
 
         # Check that source end of the plugboard is not in use.
-        if ((src in  self.wiring) or (src in self.wiring.values())):
-            raise ValueError(self.PlugboardSrcInUseMsg.format(src, dest))
+        if self._wiring[src] != src:
+            raise ValueError(self.PlugboardSrcInUseMsg.format(src.name,
+                             dest.name))
 
         # Check that destination end of the plugboard is not in use.
-        if ((dest in  self.wiring) or (dest in self.wiring.values())):
-            raise ValueError(self.PlugboardDesInUseMsg.format(src, dest))
+        if self._wiring[dest] != dest:
+            raise ValueError(self.PlugboardDesInUseMsg.format(src.name,
+                             dest.name))
 
-        self.wiring[src] = dest
+        self._wiring[src] = dest
+        self._wiring[dest] = src
 
 
     ## Get the other end of a plug.  If there is a plug then work out the other
@@ -65,8 +71,8 @@ class Plugboard:
     #  @return Other end of plug.  For example if 'A' is wired to 'T' then 'A'
     #  becomes 'T', but if 'C' isn't wired then it will return 'C'.
     def get_plug(self, src):
-        if not (src.value >= 1) and (src.value <= NO_OF_ROTOR_CONTACTS):
-            raise ValueError("Invalid source plug position")
+        if not isinstance(src, (RotorContact)):
+            raise ValueError("Invalid plug position")
 
         # The output of the plug, it will default to pass-through initially.
         plug_output = src
@@ -74,10 +80,7 @@ class Plugboard:
         # Check that the plug is fitted, if it has then modify the outgoing
         # value, otherwise it will pass straight through, e.g. 'T' will remain
         # as 'T'.
-        if src in  self.wiring:
-            plug_output = self.wiring[src]
-
-        elif src in self.wiring.values():
-            plug_output = self.wiring.keys()[self.wiring.values().index(src)]
+        if self._wiring[src] != src:
+            plug_output = self._wiring[src]
 
         return plug_output
