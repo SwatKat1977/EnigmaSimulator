c700769faa72d5c35e007fd96fbe8e6bf5af63da
Gemma Morriss <5666718+SwatKat1977@users.noreply.github.com>
2020-09-08 20:33:47 +0100

    Release Version 0.7.0 (#6)
    * Import from sourceforge (#1)
    
    * import from sourceforge
    
    * import from sourceforge
    
    * import from sourceforge
    
    * remove reference to object
    
    * Work on basic rotor functionality (#3)
    
    * rework of rotor contact to use enumeration
    
    * format change
    
    * Enigma 1 rotor wiring
    
    * removed tabs
    
    * added name to rotor
    
    * major refactoring
    
    * renamed module
    
    * made rotor factory pep 8 standard
    
    * module rename
    
    * renaming of RotorContact to rotor_contact
    
    * intermediate rename
    
    * final rename of rotor
    
    * rename
    
    * major work on rotor code
    
    * correction to copyright header
    
    * moved and renamed changelog
    
    * new change log
    
    * Work on reflector and plugboard functionality (#4)
    
    * mid rename
    
    * rename
    
    * reorganised reflector data files
    
    * remove singleton class
    
    * new base json enabled class
    
    * python 3 no longer requires __init__.py files
    
    * wip
    
    * PEP 8
    
    * PEP 8
    
    * PEP 8
    
    * PEP 8
    
    * PEP 8
    
    * work on reflector
    
    * formatting
    
    * tidy of plugboard
    
    * updated change log
    
    * renames
    
    * work on integration
    
    * rename
    
    * updates
    
    * add enigma model
    
    * implement enigma model
    
    * added Enigma models
    
    * added details for models
    
    * more info
    
    * more info
    
    * reorganisation of Enigma models
    
    * work in progress
    
    * changes
    
    * wip
    
    * wip
    
    * changes
    
    * work on Enigma machine
    
    * removed Core/machine_setup.py
    
    * fixes
    
    * Engima machine refactor (#5)
    
    * wip
    
    * wip
    
    * changes to work
    
    * simulator rums but not validated
    
    * changes to work on Enigma machine as currently doesn't work
    
    * basics are now working
    
    * ip
    
    * ip
    
    * fixed stepping
    
    * changes
    
    * tidy
    
    * change to copyright
    
    * added unit tests
    
    * fixes to plugboard
    
    * updated change log
    
    * exposed reverse wiring as read-only
    
    * wip
    
    * unit tests
    
    * wip
    
    * corrected member variable name
    
    * corrected bugs
    
    * unit tests
    
    * unit tests
    
    * bug fixes
    
    * bug fixes
    
    * unit tests
    
    * updates to changelog

---
+++ src/core_unit_tests.py
@@ -0,0 +1,26 @@
+'''
+    EnigmaSimulator - A software implementation of the Engima Machine.
+    Copyright (C) 2015-2020 Engima Simulator Development Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+'''
+import unittest
+from Core.unit_tests.enigma_machine import UnitTest_EnigmaMachine
+#from UnitTest_MachineSetup import UnitTest_MachineSetup
+from Core.unit_tests.reflector import UnitTest_Reflector
+from Core.unit_tests.plugboard import UnitTest_Plugboard
+from Core.unit_tests.reflector_factory import UnitTest_ReflectorFactory
+from Core.unit_tests.rotor import UnitTest_Rotor
+from Core.unit_tests.rotor_factory import UnitTest_RotorFactory
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/enigma_machine.py
@@ -0,0 +1,112 @@
+'''
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2015-2020 Engima Simulator Development Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+'''
+import sys
+from Core.enigma_machine import EnigmaMachine
+from Core.version import VERSION
+from Core.rotor_contact import RotorContact
+
+# https://cryptii.com/enigma-machine
+# AAAA with positions 2,3,21 should return MKIEY
+
+
+ROTORPOSITION_ONE = 0
+ROTORPOSITION_TWO = 1
+ROTORPOSITION_THREE = 2
+
+def encode_message(original_message, machine):
+    encoded_message = []
+
+    for character in original_message.upper():
+        if character < 'A' or character > 'Z':
+            print(f"[ERROR] Character '{character}' is invalid!")
+            sys.exit()
+
+        character = RotorContact[character]
+        encoded_character = machine.press_key(character)
+        encoded_message.append(encoded_character.name)
+
+    encoded_str = "".join(encoded_message)
+    return encoded_str
+
+
+def main():
+    print("Electronic Enigma Core Version V{0}".format(VERSION))
+    print("Copyright (C) 2015-2018 Electronic Engima Development Team")
+
+    try:
+        enigma_machine = EnigmaMachine('Enigma1')
+
+    except ValueError as err:
+        print(f'[ERROR] {err}')
+        return
+
+    config_return = enigma_machine.configure_machine(['I', 'II', 'III'], 'Wide_B')
+
+    if not config_return:
+        print(f'[ERROR] {enigma_machine.last_error}')
+        return
+
+    #enigma_machine.debug_messages = True
+
+    # # Set the rotor positions
+    # enigma_machine.set_rotor_position(ROTORPOSITION_ONE, 1)
+    # enigma_machine.set_rotor_position(ROTORPOSITION_TWO, 1)
+    # enigma_machine.set_rotor_position(ROTORPOSITION_THREE, 1)
+
+    # # machine.GetRotor(0).RingSetting = 2
+    # # machine.GetRotor(1).RingSetting = 2
+    # # machine.GetRotor(2).RingSetting = 2
+
+    # string_to_encode = "AAAAA"
+    # encoded = encode_message(string_to_encode, enigma_machine)
+    # print("+----------------------------------------------")
+    # print(f"Encoded string '{string_to_encode} as : {encoded}")
+    # print(f"=> Rotor 1 Position: {enigma_machine.get_rotor_position(0)}")
+    # print(f"=> Rotor 2 Position: {enigma_machine.get_rotor_position(1)}")
+    # print(f"=> Rotor 3 Position: {enigma_machine.get_rotor_position(2)}")
+    # print("+----------------------------------------------")
+
+    # AAAA with positions 2,3,21 should return MKIEY
+
+    # Set the rotor positions
+    enigma_machine.set_rotor_position(ROTORPOSITION_ONE, 2)
+    enigma_machine.set_rotor_position(ROTORPOSITION_TWO, 3)
+    enigma_machine.set_rotor_position(ROTORPOSITION_THREE, 21)
+
+    enigma_machine.set_rotor_position(ROTORPOSITION_ONE, 1)
+    enigma_machine.set_rotor_position(ROTORPOSITION_TWO, 1)
+    enigma_machine.set_rotor_position(ROTORPOSITION_THREE, 1)
+    #enigma_machine.set_rotor_position(ROTORPOSITION_THREE, RotorContact.U.value)
+
+    string_to_encode = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    encoded = encode_message(string_to_encode, enigma_machine)
+    print("+----------------------------------------------")
+    print(f"Encoded string '{string_to_encode} as : {encoded}")
+    print(f"=> Rotor 1 Position: {RotorContact(enigma_machine.get_rotor_position(0)).name}")
+    print(f"=> Rotor 2 Position: {RotorContact(enigma_machine.get_rotor_position(1)).name}")
+    print(f"=> Rotor 3 Position: {RotorContact(enigma_machine.get_rotor_position(2)).name}")
+    print("+----------------------------------------------")
+
+    enigma_machine.set_rotor_position(ROTORPOSITION_ONE, RotorContact(2).value)
+    enigma_machine.set_rotor_position(ROTORPOSITION_TWO, RotorContact(3).value)
+    enigma_machine.set_rotor_position(ROTORPOSITION_THREE, RotorContact(21).value)
+
+    string_to_encode = "AAAA"
+    encoded = encode_message(string_to_encode, enigma_machine)
+    print(f"Encoded string '{string_to_encode} as : {encoded}")
+
+
+if __name__ == '__main__':
+    main()
