e741faf5caf2cab31a9eae7c51d4764a1b40fa55
Gemma Morriss <paulmorriss1977@gmail.com>
2024-02-01 23:55:03 +0000

    rotor contact enumeration

---
+++ src/libEnigmaSimulator/RotorContact.h
@@ -0,0 +1,53 @@
+/*
+    Engima Machine Simulator
+    Copyright (C) 2015-2024 Engima Simulator Development Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+#ifndef ROTORCONTACT_H
+#define ROTORCONTACT_H
+
+namespace enigmaSimualator {
+
+    // Enumeration of a rotor contact character (A-Z)
+    enum RotorContact
+    {
+        kRotorContactA = 0,
+        kRotorContactB = 1,
+        kRotorContactC = 2,
+        kRotorContactD = 3,
+        kRotorContactE = 4,
+        kRotorContactF = 5,
+        kRotorContactG = 6,
+        kRotorContactH = 7,
+        kRotorContactI = 8,
+        kRotorContactJ = 9,
+        kRotorContactK = 10,
+        kRotorContactL = 11,
+        kRotorContactM = 12,
+        kRotorContactN = 13,
+        kRotorContactO = 14,
+        kRotorContactP = 15,
+        kRotorContactQ = 16,
+        kRotorContactR = 17,
+        kRotorContactS = 18,
+        kRotorContactT = 19,
+        kRotorContactU = 20,
+        kRotorContactV = 21,
+        kRotorContactW = 22,
+        kRotorContactX = 23,
+        kRotorContactY = 24,
+        kRotorContactZ = 25
+    };
+
+}   // namespace enigmaSimualator
+
+#endif  //  #ifndef ROTORCONTACT_H
