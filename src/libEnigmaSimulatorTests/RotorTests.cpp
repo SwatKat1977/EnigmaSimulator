5a71ca69cb2123951cb957fc8d17cc1d61dd2f7c
SwatKat1977 <paulmorriss1977@gmail.com>
2024-02-06 22:40:22 +0000

    unit tests

---
+++ src/libEnigmaSimulatorTests/RotorTests.cpp
@@ -0,0 +1,58 @@
+#include "gtest/gtest.h"
+#include "Rotor.h"
+#include "RotorTest.h"
+
+int cubic(int pop)
+{
+    return 0;
+}
+
+TEST_F(RotorTest, ConstructWithValidWiring)
+{
+    try
+    {
+        auto rotor = enigmaSimulator::Rotor(
+            "Rotor 1",
+            valid_wiring_layout_,
+            std::vector<enigmaSimulator::RotorContact>());
+    }
+    catch(const std::exception& e)
+    {
+        FAIL() << "Expected valid rotor wiring";
+    }
+}
+
+TEST_F(RotorTest, ConstructWithInvalidWiring)
+{
+    try
+    {
+        auto rotor = enigmaSimulator::Rotor(
+            "Rotor 1",
+            invalid_wiring_layout_,
+            std::vector<enigmaSimulator::RotorContact>());
+        FAIL() << "Expected invalid rotor wiring";
+    }
+    catch(const std::exception& e)
+    {
+    }
+}
+
+TEST_F(RotorTest, EncryptNoRotorRingOrPositionOffsets)
+{
+    auto rotor = enigmaSimulator::Rotor(
+        "Rotor 1",
+        valid_wiring_layout_,
+        std::vector<enigmaSimulator::RotorContact>());
+
+    auto output = rotor.Encrypt(enigmaSimulator::kRotorContact_A);
+
+    ASSERT_EQ(output, enigmaSimulator::kRotorContact_E);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+/// g++ foo.cpp -o yup -std=c++17 -I /opt/homebrew/include -lgtest -L/opt/homebrew/lib -ftest-coverage
