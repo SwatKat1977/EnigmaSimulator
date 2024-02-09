cea3e6ed1b4de195e2bc3d467165c874e80dd64a
Gemma Morriss <paulmorriss1977@gmail.com>
2024-02-09 22:53:30 +0000

    work on setting up reflectors

--- src/libEnigmaSimulator/ReflectorFactory.cpp
+++ src/libEnigmaSimulator/ReflectorFactory.cpp
@@ -12,113 +12,25 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 */
+#include <map>
+#include "ReflectorFactory.h"
 
 namespace enigmaSimulator {
 
+std::map<std::string, std::string> REFLECTORS =
+{
+    // Enigma I
+    { ENIGMA_1_REFLECTOR_UKW_A, "EJMZALYXVBWFCRQUONTSPIKHGD" },
+    { ENIGMA_1_REFLECTOR_UKW_B, "YRUHQSLDPXNGOKMIEBFZCWVJAT" },
+    { ENIGMA_1_REFLECTOR_UKW_C, "FVPJIAOYEDRZXWGCTKUQSBNMHL" },
+
+    // Enigma Model M3
+    { ENIGMA_M3_REFLECTOR_UKW_B, "YRUHQSLDPXNGOKMIEBFZCWVJAT" },
+    { ENIGMA_M3_REFLECTOR_UKW_C, "FVPJIAOYEDRZXWGCTKUQSBNMHL" },
+
+    // German Navy 4-rotor M4 Enigma
+    { ENIGMA_M4_REFLECTOR_UKW_B, "ENKQAUYWJICOPBLMDXZVFTHRGS" },
+    { ENIGMA_M4_REFLECTOR_UKW_C, "RDOBJNTKVEHMLFCWZAXGYIPSUQ" }
+};
+
 }   // namespace enigmaSimulator
-
-#ifdef __OLD__
-    // Enigma Model 1
-const std::string ENIGMA_1_REFLECTOR_UKW_A = "Enigma1_Reflector_UKW-A";
-const std::string ENIGMA_1_REFLECTOR_UKW_B = "Enigma1_Reflector_UKW-B";
-const std::string ENIGMA_1_REFLECTOR_UKW_C = "Enigma1_Reflector_UKW-C";
-
-// Enigma Model M3
-const std::string ENIGMA_M3_REFLECTOR_UKW_B = "EnigmaM3_Reflector_UKW-B";
-const std::string ENIGMA_M3_REFLECTOR_UKW_C = "EnigmaM3_Reflector_UKW-C";
-
-// Enigma Model M4
-const std::string ENIGMA_M4_REFLECTOR_UKW_B = "EnigmaM4_Reflector_UKW-B";
-const std::string ENIGMA_M4_REFLECTOR_UKW_C = "EnigmaM4_Reflector_UKW-C";
-
-#endif
-
-#ifdef __OLD_CODE__
-ENIGMA_MODELS = {
-    # The Enigma I was the main Enigma machine used by the German Army and Air
-    # Force.The military machines were the only ones with a plug board.The ETW
-    # and all three known UKWs.UKW - A was used before WWII.UKW - B was the
-    # standard reflector during the war and UKW - C was only used in the later part
-    # of the war.The wiring of the five wheels is identical to the wiring of the
-    # first 5 wheels of the Enigma M3 (Navy) and the U - Boot Enigma M4.
-    'Enigma1' : EnigmaModel
-    (
-        'Enigma Model 1',
-        'Enigma1',
-        RotorCount.THREE,
-        True,
-        [
-            RotorDefintion ('I',   'EKMFLGDQVZNTOWYHXUSPAIBRCJ',['Q']),
-            RotorDefintion ('II',  'AJDKSIRUXBLHWTMCQGZNPYFVOE',['E']),
-            RotorDefintion ('III', 'BDFHJLCPRTXVZNYEIWGAKMUSQO',['V']),
-            RotorDefintion ('IV',  'ESOVPZJAYQUIRHXLNFTGKDCMWB',['J']),
-            RotorDefintion ('V',   'VZBRGITYUPSDNHLXAWMJQOFECK',['Z'])
-        ],
-        [
-            ReflectorDefinition ('UKW-A', 'EJMZALYXVBWFCRQUONTSPIKHGD'),
-            ReflectorDefinition ('UKW-B', 'YRUHQSLDPXNGOKMIEBFZCWVJAT'),
-            ReflectorDefinition ('UKW-C', 'FVPJIAOYEDRZXWGCTKUQSBNMHL')
-        ]
-    ),
-
-    # The Enigma M1, M2 and M3 machines were used by the German Navy
-    # (Kriegsmarine). They are basically compatible with the Enigma I. The
-    # wiring of the Enigma M3 is given in the table below.Wheels I thru V are
-    # identical to those of the Enigma I.The same is true for UKW B and C.The
-    # three additional wheels (VI, VII and VIII) were used exclusively by the
-    # Kriegsmarine.The machine is also compatible with the Enigma M4 (when the
-    # 4th wheel of the M4 is set to position 'A').
-    'M3' : EnigmaModel
-    (
-        'Enigma Model M3',
-        'M3',
-        RotorCount.THREE,
-        True,
-        [
-            RotorDefintion ('I',    'EKMFLGDQVZNTOWYHXUSPAIBRCJ',['Q']),
-            RotorDefintion ('II',   'AJDKSIRUXBLHWTMCQGZNPYFVOE',['E']),
-            RotorDefintion ('III',  'BDFHJLCPRTXVZNYEIWGAKMUSQO',['V']),
-            RotorDefintion ('IV',   'ESOVPZJAYQUIRHXLNFTGKDCMWB',['J']),
-            RotorDefintion ('V',    'VZBRGITYUPSDNHLXAWMJQOFECK',['Z']),
-            RotorDefintion ('VI',   'JPGVOUMFYQBENHZRDKASXLICTW',['Z', 'M']),
-            RotorDefintion ('VII',  'NZJHGRCXMYSWBOUFAIVLPEKQDT',['Z', 'M']),
-            RotorDefintion ('VIII', 'FKQHTLXOCBJSPDZRAMEWNIUYGV',['Z', 'M'])
-        ],
-        [
-            ReflectorDefinition ('UKW-B', 'YRUHQSLDPXNGOKMIEBFZCWVJAT'),
-            ReflectorDefinition ('UKW-C', 'FVPJIAOYEDRZXWGCTKUQSBNMHL')
-        ]
-    ),
-
-    # The Enigma M4 was a further development of the M3 and was used
-    # exclusively by the U - boat division of the German Navy (Kriegsmarine).It
-    # was introduced unexpectedly on 2 February 1942. UKW - B was the standard
-    # reflector throughout the war and UKW - C was only temporarily used during
-    # the war.The wiring of the first 5 wheels (I - V)is identical to the wiring
-    # of the 5 wheels of the Enigma I, that was used by the Wehrmacht and
-    # Luftwaffe.This allowed secure communication between the departments.
-    # Bletchley Park called the messages to and from Atlantic U - boats by the
-    # code name "Shark", the rest of the traffic they called "Dolphin.
-    'M4' : EnigmaModel
-    (
-        'German Navy 4-rotor M4 Enigma',
-        'M4',
-        RotorCount.FOUR,
-        True,
-        [
-            RotorDefintion ('I',    'EKMFLGDQVZNTOWYHXUSPAIBRCJ',['Q']),
-            RotorDefintion ('II',   'AJDKSIRUXBLHWTMCQGZNPYFVOE',['E']),
-            RotorDefintion ('III',  'BDFHJLCPRTXVZNYEIWGAKMUSQO',['V']),
-            RotorDefintion ('IV',   'ESOVPZJAYQUIRHXLNFTGKDCMWB',['J']),
-            RotorDefintion ('V',    'VZBRGITYUPSDNHLXAWMJQOFECK',['Z']),
-            RotorDefintion ('VI',   'JPGVOUMFYQBENHZRDKASXLICTW',['Z', 'M']),
-            RotorDefintion ('VII',  'NZJHGRCXMYSWBOUFAIVLPEKQDT',['Z', 'M']),
-            RotorDefintion ('VIII', 'FKQHTLXOCBJSPDZRAMEWNIUYGV',['Z', 'M'])
-        ],
-        [
-            ReflectorDefinition ('UKW-B', 'ENKQAUYWJICOPBLMDXZVFTHRGS'),
-            ReflectorDefinition ('UKW-C', 'RDOBJNTKVEHMLFCWZAXGYIPSUQ')
-        ]
-    )
-}
-#endif
