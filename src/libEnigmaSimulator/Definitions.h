0b5fa21e43c2bead9bdcd9d9c7f8705c4696c31e
Gemma Morriss <paulmorriss1977@gmail.com>
2024-02-19 18:26:41 +0000

    common definitions file

---
+++ src/libEnigmaSimulator/Definitions.h
@@ -0,0 +1,24 @@
+/*
+    Engima Machine Simulator
+    Copyright (C) 2015-2024 Engima Simulator Development Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+namespace enigmaSimulator {
+
+    const int MAX_WIRING_ENTRIES = 26;
+
+}   // namespace enigmaSimulator
+
+#endif  //  #ifndef DEFINITIONS_H
